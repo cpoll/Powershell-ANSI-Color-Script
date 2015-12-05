dbf8d890ee8daddbb1545d52a3a122743909fcee
Cristian Poll <cpoll24@gmail.com>
2015-12-05 10:54:24 -0500

    add ansi color enum

--- AnsiColorize.psm1
+++ AnsiColorize.psm1
@@ -11,16 +11,17 @@ function Colorize-Text {
     .PARAMETER ansiSgrCode
         ANSI SGR number to insert.
         See https://en.wikipedia.org/wiki/ANSI_escape_code for details
+        Or use the [AnsiColor] enum.
         Also accepts an array of SGR numbers, and will apply all of them.
         
     .NOTES
         Designed to play nicely with https://wiki.jenkins-ci.org/display/JENKINS/AnsiColor+Plugin
   
     .EXAMPLE
-        Colorize-Text -text "Hello World." -ansiSgrCode 33
+        Colorize-Text -text "Hello World." -ansiSgrCode ([AnsiColor]::ForegroundRed -as [int])
     
     .EXAMPLE
-        Colorize-Text -text "Hello World." -ansiSgrCode @(33, 44)
+        Colorize-Text -text "Hello World." -ansiSgrCode @( ([AnsiColor]::ForegroundRed -as [int]), ([AnsiColor]::BackgroundYellow -as [int]) )
     #>
     param(
         $text,
@@ -38,3 +39,24 @@ function Colorize-Text {
 
     return [char]27 + "$ansiCodeString$text" + [char]27 + "[0m"
 }
+
+Add-Type -TypeDefinition @"
+    public enum AnsiColor {
+        ForegroundBlack = 30,
+        ForegroundRed = 31,
+        ForegroundGreen = 32,
+        ForegroundYellow = 33,
+        ForegroundBlue = 34,
+        ForegroundMagenta = 35,
+        ForegroundCyan = 36,
+        ForegroundWhite = 37,
+        BackgroundBlack = 40,
+        BackgroundRed = 41,
+        BackgroundGreen = 42,
+        BackgroundYellow = 43,
+        BackgroundBlue = 44,
+        BackgroundMagenta = 45,
+        BackgroundCyan = 46,
+        BackgroundWhite = 47,
+    }
+"@
