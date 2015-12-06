fb18b7152ec798295a9beb19879e9910f871ae67
Cristian Poll <cpoll24@gmail.com>
2015-12-06 08:44:10 -0500

    add Write-HostAnsi and helpers

--- AnsiColorize.psm1
+++ AnsiColorize.psm1
@@ -1,3 +1,36 @@
+function Write-HostAnsi {
+     <#
+        .SYNOPSIS
+            Wrapper for Write-Host, adds ANSI SGR codes based on -foregroundColor and -backgroundColor
+    #>
+
+    param(
+        $object,
+        $foregroundColor,
+        $backgroundColor
+    )
+    
+    $writeHostSplatArgs = @{}
+    $ansiSgrCodes = @()
+
+    if($foregroundColor -ne $null){
+        $ansiSgrCodes += Convert-ForegroundColorToAnsi $foregroundColor
+        $writeHostSplatArgs.add("foregroundColor", $foregroundColor)
+    }
+
+    if($backgroundColor -ne $null){
+        $ansiSgrCodes += Convert-BackgroundColorToAnsi $backgroundColor
+        $writeHostSplatArgs.add("backgroundColor", $backgroundColor)
+    }
+
+    if($foregroundColor -ne $null -or $backgroundColor -ne $null){
+        $newObject = Colorize-Text -text $object -ansiSgrCode $ansiSgrCodes
+        Write-Host $newObject @writeHostSplatArgs @args
+    } else {
+        Write-Host $object @args
+    }
+}
+
 function Colorize-Text {
     <#
     .SYNOPSIS
@@ -12,6 +45,7 @@ function Colorize-Text {
         ANSI SGR number to insert.
         See https://en.wikipedia.org/wiki/ANSI_escape_code for details
         Or use the [AnsiColor] enum.
+
         Also accepts an array of SGR numbers, and will apply all of them.
         
     .NOTES
@@ -22,6 +56,7 @@ function Colorize-Text {
     
     .EXAMPLE
         Colorize-Text -text "Hello World." -ansiSgrCode @( ([AnsiColor]::ForegroundRed -as [int]), ([AnsiColor]::BackgroundYellow -as [int]) )
+
     #>
     param(
         $text,
@@ -60,3 +95,66 @@ Add-Type -TypeDefinition @"
         BackgroundWhite = 47,
     }
 "@
+
+$foregroundColorAnsiCodes = @{
+    #https://msdn.microsoft.com/en-us/library/system.consolecolor(v=vs.110).aspx
+    #Converted to closest ANSI SGR equivalent
+    "Black" = [AnsiColor]::ForegroundBlack -as [int];
+    "White" = [AnsiColor]::ForegroundWhite -as [int];
+    "Blue" = [AnsiColor]::ForegroundBlue -as [int];
+    "DarkBlue" = [AnsiColor]::ForegroundBlue -as [int];
+    "Green" = [AnsiColor]::ForegroundGreen -as [int];
+    "DarkGreen" = [AnsiColor]::ForegroundGreen -as [int];
+    "Cyan" = [AnsiColor]::ForegroundCyan -as [int];
+    "DarkCyan" = [AnsiColor]::ForegroundCyan -as [int];
+    "Red" = [AnsiColor]::ForegroundRed -as [int];
+    "DarkRed" = [AnsiColor]::ForegroundRed -as [int];
+    "Magenta" = [AnsiColor]::ForegroundMagenta -as [int];
+    "DarkMagenta" = [AnsiColor]::ForegroundMagenta -as [int];
+    "Yellow" = [AnsiColor]::ForegroundYellow -as [int];
+    "DarkYellow" = [AnsiColor]::ForegroundYellow -as [int];
+
+    #No equivalent
+    "Gray" = [AnsiColor]::ForegroundBlack -as [int];
+    "DarkGray" = [AnsiColor]::ForegroundBlack -as [int];
+}
+
+$backgroundColorAnsiCodes = @{
+    #https://msdn.microsoft.com/en-us/library/system.consolecolor(v=vs.110).aspx
+    #Converted to closest ANSI SGR equivalent
+    "Black" = [AnsiColor]::BackgroundBlack -as [int];
+    "White" = [AnsiColor]::BackgroundWhite -as [int];
+    "Blue" = [AnsiColor]::BackgroundBlue -as [int];
+    "DarkBlue" = [AnsiColor]::BackgroundBlue -as [int];
+    "Green" = [AnsiColor]::BackgroundGreen -as [int];
+    "DarkGreen" = [AnsiColor]::BackgroundGreen -as [int];
+    "Cyan" = [AnsiColor]::BackgroundCyan -as [int];
+    "DarkCyan" = [AnsiColor]::BackgroundCyan -as [int];
+    "Red" = [AnsiColor]::BackgroundRed -as [int];
+    "DarkRed" = [AnsiColor]::BackgroundRed -as [int];
+    "Magenta" = [AnsiColor]::BackgroundMagenta -as [int];
+    "DarkMagenta" = [AnsiColor]::BackgroundMagenta -as [int];
+    "Yellow" = [AnsiColor]::BackgroundYellow -as [int];
+    "DarkYellow" = [AnsiColor]::BackgroundYellow -as [int];
+
+    #No equivalent
+    "Gray" = [AnsiColor]::BackgroundBlack -as [int];
+    "DarkGray" = [AnsiColor]::BackgroundBlack -as [int];
+}
+
+function Convert-ForegroundColorToAnsi {
+    param(
+        $foregroundColor 
+    )
+
+    return $foregroundColorAnsiCodes[$foregroundColor]
+}
+
+function 
+Convert-BackgroundColorToAnsi {
+    param(
+        $backgroundColor
+    )
+
+    return $backgroundColorAnsiCodes[$backgroundColor]
+}
